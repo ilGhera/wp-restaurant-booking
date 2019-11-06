ff31238b7263983ad2c1f37f16a3b3fd6ec8ba2e
ilGhera <andrea@ilghera.com>
2019-11-06 08:57:14 +0100

    Colonne personalizzate in reservation posts table

--- includes/class-wprb-reservations.php
+++ includes/class-wprb-reservations.php
@@ -20,6 +20,11 @@ class WPRB_Reservations {
 			add_action( 'init', array( $this, 'register_post_type' ) );
 			add_action( 'add_meta_boxes', array( $this, 'wprb_add_meta_box' ) );
 			add_action( 'save_post', array( __CLASS__, 'save_single_reservation' ), 10, 1 );
+			add_filter( 'manage_edit-reservation_columns', array( $this, 'edit_reservation_columns' ) ) ;
+			add_action( 'manage_reservation_posts_custom_column', array( $this, 'manage_reservation_columns' ), 10, 2 );
+			add_filter( 'manage_edit-reservation_sortable_columns', array( $this, 'reservation_sortable_columns' ) );
+			add_action( 'load-edit.php', array( $this, 'edit_reservations_load' ) );
+
 
 		}
 
@@ -162,6 +167,138 @@ class WPRB_Reservations {
 			}
 
 		}
+
+	}
+
+
+	/**
+	 * Customize the reservation post table columns
+	 * 
+	 * @param  array $columns the default WP table columns.
+	 * @return array          the updated columns
+	 */
+	public function edit_reservation_columns( $columns ) {
+
+		$columns = array(
+			'cb'     => '&lt;input type="checkbox" />',
+			'title'  => __( 'Title' ),
+			'day'    => __( 'Day', 'wprb' ),
+			'time'   => __( 'Time', 'wprb' ),
+			'people' => __( 'People', 'wprb' ),
+			'table'  => __( 'Table', 'wprb' ),
+			// 'date'   => __( 'Date' )
+		);
+
+		return $columns;
+
+	}
+
+
+	/**
+	 * Manage the content of the reservatiosn post table columns
+	 * 
+	 * @param  string $column the column name.
+	 * @param  int    $post_id the reservations id.
+	 * @return mixed
+	 */
+	public function manage_reservation_columns( $column, $post_id ) {
+
+		global $post;
+
+		switch ( $column ) {
+			
+			case 'day':
+				
+				$day = get_post_meta( $post_id, 'wprb-date', true );
+				
+				echo $day;
+				
+				break;
+		
+			case 'time':
+				
+				$time = get_post_meta( $post_id, 'wprb-time', true );
+
+				echo $time;
+				
+				break;
+		
+			case 'people':
+				
+				$people = get_post_meta( $post_id, 'wprb-people', true );
+				
+				echo $people;
+				
+				break;
+
+			case 'table':
+
+				$table = get_post_meta( $post_id, 'wprb-table', true );
+			
+				echo $table ? $table : __( 'No table assigned', 'wprb' );
+
+				break;
+			
+			default:
+
+				break;
+		
+		}
+
+	}
+
+
+	/**
+	 * Make the custom columns sortable
+	 * 
+	 * @param  array $columns the table columns.
+	 * @return array
+	 */
+	public function reservation_sortable_columns( $columns ) {
+
+		$columns['day'] = 'day';
+
+		return $columns;
+
+	}
+
+
+	/**
+	 * Activate the filter only in the edit page in the admin
+	 */
+	public function edit_reservations_load() {
+
+		add_filter( 'request', array( $this, 'my_sort_reservations' ) );
+
+	}
+
+
+	/**
+	 * Sort reservations by day
+	 * 
+	 * @param  array $vars the query.
+	 * @return array       the query updated
+	 */
+	public function my_sort_reservations( $vars ) {
+
+		/* Check if we're viewing the 'reservation' post type. */
+		if ( isset( $vars['post_type'] ) && 'reservation' === $vars['post_type'] ) {
+
+			/* Check if 'orderby' is set to 'duration'. */
+			if ( isset( $vars['orderby'] ) && 'day' === $vars['orderby'] ) {
+
+				/* Merge the query vars with our custom variables. */
+				$vars = array_merge(
+					$vars,
+					array(
+						'meta_key' => 'wprb-date',
+						'orderby' => 'meta_value'
+					)
+				);
+			}
+		}
+
+		return $vars;
 
 	}
 
