2cbd8809b5f586d6f93b2e91bc2e348e176a8712
ilGhera <andrea@ilghera.com>
2019-12-09 21:53:14 +0100

    Aggiunto plugin update checker

--- wp-restaurant-booking.php
+++ wp-restaurant-booking.php
@@ -14,6 +14,7 @@
 
 /**
  * Handles the plugin activation
+ *
  * @return void
  */
 function load_wp_restaurant_booking() {
@@ -32,6 +33,7 @@ function load_wp_restaurant_booking() {
 	define( 'WPRB_INCLUDES', WPRB_DIR . 'includes/' );
 	define( 'WPRB_ADMIN', WPRB_DIR . 'admin/' );
 	define( 'WPRB_SETTINGS', admin_url( 'admin.php?page=wp-restaurant-booking' ) );
+	
 
 	/*Files required*/
 	require( WPRB_ADMIN . 'class-wprb-admin.php' );
@@ -44,3 +46,39 @@ function load_wp_restaurant_booking() {
 
 }
 add_action( 'after_setup_theme', 'load_wp_restaurant_booking', 10 );
+
+/**
+ * Update checker Builder
+ */
+require( plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php' );
+$wprb_update_checker = Puc_v4_Factory::buildUpdateChecker(
+	'https://www.ilghera.com/wp-update-server-2/?action=get_metadata&slug=wp-restaurant-booking-premium',
+	__FILE__,
+	'wp-restaurant-booking-premium'
+);
+
+/**
+ * Add secure check filter
+ */
+$wprb_update_checker->addQueryArgFilter( 'wprb_secure_update_check' );
+
+/**
+ * Add Premium Key to update data
+ *
+ * @param  array $query_args update data.
+ * @return array
+ */
+function wprb_secure_update_check( $query_args ) {
+
+	$key = base64_encode( get_option( 'wprb-premium-key' ) );
+
+	if ( $key ) {
+
+		$query_args['premium-key'] = $key;
+
+	}
+
+	return $query_args;
+
+}
+
